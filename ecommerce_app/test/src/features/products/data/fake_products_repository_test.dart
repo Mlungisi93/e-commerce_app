d1b6e3cf31558bfc3674bb64ce1571ca0aa44475
Mlungisi93 <215229827@edu.vut.ac.za>
2024-09-04 08:50:06 +0200

    07 Add remaining tests for Futures and Streams to complete Product repository test except for providers as they do not have business logic

--- ecommerce_app/test/src/features/products/data/fake_products_repository_test.dart
+++ ecommerce_app/test/src/features/products/data/fake_products_repository_test.dart
@@ -3,23 +3,54 @@ import 'package:ecommerce_app/src/features/products/data/fake_products_repositor
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('getProductsList returns global product list', () {
-    final productsRepository = FakeProductsRepository();
-    expect(productsRepository.getProductsList(), kTestProducts);
-  });
-  test('getProduct(1) returns first item', () {
-    final productsRepository = FakeProductsRepository();
-    expect(
-      productsRepository.getProduct('1'),
-      kTestProducts[0],
-    );
+  group('FakeProductsRepository', () {
+    test('getProductsList returns global product list', () {
+      final productsRepository = FakeProductsRepository();
+      expect(productsRepository.getProductsList(), kTestProducts);
+    });
+    test('getProduct(1) returns first item', () {
+      final productsRepository = FakeProductsRepository();
+      expect(
+        productsRepository.getProduct('1'),
+        kTestProducts[0],
+      );
+    });
+
+    test('getProduct(100) returns null', () {
+      final productsRepository = FakeProductsRepository();
+      expect(
+        productsRepository.getProduct('100'),
+        null,
+      );
+    });
   });
 
-  test('getProduct(100) returns null', () {
+  test('fetchProductsList future returns global list', () async {
     final productsRepository = FakeProductsRepository();
     expect(
-      productsRepository.getProduct('100'),
-      null,
+      await productsRepository.fetchProductsList(),
+      kTestProducts,
+    );
+  });
+  test('watchProductsList emits global list', () {
+    final productsRepository = FakeProductsRepository();
+    expect(
+      productsRepository.watchProductsList(),
+      emits(kTestProducts),
+    );
+  });
+  test('watchProduct(1) stream emits first item', () {
+    final productsRepository = FakeProductsRepository();
+    expect(
+      productsRepository.watchProduct('1'),
+      emits(kTestProducts[0]),
+    );
+  });
+  test('watchProduct(100) stream emits null', () {
+    final productsRepository = FakeProductsRepository();
+    expect(
+      productsRepository.watchProduct('100'),
+      emits(null),
     );
   });
 }
