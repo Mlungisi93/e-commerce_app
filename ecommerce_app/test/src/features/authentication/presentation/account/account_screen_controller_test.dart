d56f16e6be1d8e0330c562f24aa6e9d6bfa8c050
Mlungisi93 <215229827@edu.vut.ac.za>
2024-09-06 17:02:20 +0200

    16 we want to test the state changes over time solution- Testing with Stream Matchers and Predicates

--- ecommerce_app/test/src/features/authentication/presentation/account/account_screen_controller_test.dart
+++ ecommerce_app/test/src/features/authentication/presentation/account/account_screen_controller_test.dart
@@ -17,45 +17,59 @@ void main() {
       expect(controller.state, const AsyncData<void>(null));
     });
 
-    test('signOut success', () async {
-      // setup
-      final authRepository = MockAuthRepository();
-      /*By default, mocks implement all methods by returning null.
-
-Stubbing a mock
-We can stub a mock to decide the response of a mock method. Example: */
-      when(authRepository.signOut).thenAnswer(
-        (_) => Future.value(),
-      );
-      final controller = AccountScreenController(
-        authRepository: authRepository,
-      );
-      // run
-      await controller.signOut();
-      // verify
-      verify(authRepository.signOut).called(1);
-      expect(controller.state, const AsyncData<void>(null));
-    });
-
-    test('signOut failure', () async {
-      // setup
-      final authRepository = MockAuthRepository();
-      final exception = Exception('Connection failed');
-      when(authRepository.signOut).thenThrow(exception);
-      final controller = AccountScreenController(
-        authRepository: authRepository,
-      );
-      // run
-      await controller.signOut();
-      // verify
-      verify(authRepository.signOut).called(1);
-      expect(controller.state.hasError,
-          true); //verify that only error without Staketrace
-      expect(controller.state, isA<AsyncError>());
-      // expect(controller.state, AsyncError<void>(exception, StackTrace.empty));
-
-      ////this fails because syncValue.guard(() throws both error and Stacktrace(while we were expection stacktrace to be null but actually is not null)
-      //matchers
-    });
+    test(
+      'signOut success',
+      () async {
+        // setup
+        final authRepository = MockAuthRepository();
+        when(authRepository.signOut).thenAnswer(
+          (_) => Future.value(),
+        );
+        final controller = AccountScreenController(
+          authRepository: authRepository,
+        );
+        // expect later
+        expectLater(
+          controller.stream,
+          emitsInOrder(const [
+            AsyncLoading<void>(),
+            AsyncData<void>(null),
+          ]),
+        );
+        // run
+        await controller.signOut();
+        // verify
+        verify(authRepository.signOut).called(1);
+      },
+      timeout: const Timeout(Duration(milliseconds: 500)),
+    );
+    test(
+      'signOut failure',
+      () async {
+        // setup
+        final authRepository = MockAuthRepository();
+        final exception = Exception('Connection failed');
+        when(authRepository.signOut).thenThrow(exception);
+        final controller = AccountScreenController(
+          authRepository: authRepository,
+        );
+        // expect later
+        expectLater(
+          controller.stream,
+          emitsInOrder([
+            const AsyncLoading<void>(),
+            predicate<AsyncValue<void>>((value) {
+              expect(value.hasError, true);
+              return true;
+            }),
+          ]),
+        );
+        // run
+        await controller.signOut();
+        // verify
+        verify(authRepository.signOut).called(1);
+      },
+      timeout: const Timeout(Duration(milliseconds: 500)),
+    );
   });
 }
