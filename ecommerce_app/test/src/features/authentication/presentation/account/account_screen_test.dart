9ee07b6af5b7e01149bea87080045449954c3f60
Mlungisi93 <215229827@edu.vut.ac.za>
2024-09-11 08:01:09 +0200

    04 Working with WidgetTester and finder

---
+++ ecommerce_app/test/src/features/authentication/presentation/account/account_screen_test.dart
@@ -0,0 +1,33 @@
+import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Cancel Logout', (tester) async {
+    // pump to trigger new frame
+    await tester.pumpWidget(const ProviderScope(
+      child: MaterialApp(
+        home: AccountScreen(),
+      ),
+    ));
+    // find logout button and tap it
+    final logoutButton = find.text('Logout');
+    expect(logoutButton, findsOneWidget);
+    await tester.tap(logoutButton); //to interact with the finder
+    await tester.pump();
+
+    // expect that the logout dialog is presented
+    final dialogTitle = find.text('Are you sure?');
+    expect(dialogTitle, findsOneWidget);
+
+    // find cancel button and tap it
+    final cancelButton = find.text('Cancel');
+    expect(cancelButton, findsOneWidget);
+    await tester.tap(cancelButton);
+    await tester.pump();
+
+    // expect that the logout dialog is no longer visible
+    expect(dialogTitle, findsNothing);
+  });
+}
