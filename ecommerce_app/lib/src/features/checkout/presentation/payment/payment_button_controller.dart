c1b586cb62cdd799f0e1d6fca7d29d694afa0096
Mlungisi93 <215229827@edu.vut.ac.za>
2025-01-24 00:31:05 +0200

    Module 13 converting state notifier to async notifier practice: still out standing is ShoppingCartScreenController and LeaveReviewController

--- ecommerce_app/lib/src/features/checkout/presentation/payment/payment_button_controller.dart
+++ ecommerce_app/lib/src/features/checkout/presentation/payment/payment_button_controller.dart
@@ -1,6 +1,35 @@
-import 'package:ecommerce_app/src/features/checkout/application/fake_checkout_service.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'dart:async';
 
+import 'package:ecommerce_app/src/features/checkout/application/fake_checkout_service.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'payment_button_controller.g.dart';
+
+@riverpod
+class PaymentButtonController extends _$PaymentButtonController {
+  bool _mounted = true;
+
+  @override
+  FutureOr<void> build() {
+    ref.onDispose(() => _mounted = false);
+  }
+
+  Future<void> pay() async {
+    final checkoutService = ref.read(checkoutServiceProvider);
+    state = const AsyncLoading();
+    final newState = await AsyncValue.guard(checkoutService.placeOrder);
+    // * Check if the controller is mounted before setting the state to prevent:
+    // * Bad state: Tried to use PaymentButtonController after `dispose` was called.
+    if (_mounted) {
+      state = newState;
+    }
+  }
+}
+
+
+/* 
 class PaymentButtonController extends StateNotifier<AsyncValue<void>> {
   PaymentButtonController({required this.checkoutService})
       : super(const AsyncData(null));
@@ -23,3 +52,4 @@ final paymentButtonControllerProvider = StateNotifierProvider.autoDispose<
     checkoutService: ref.watch(checkoutServiceProvider),
   );
 });
+*/
